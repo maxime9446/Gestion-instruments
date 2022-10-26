bf945ed10d776abc110952a553545884ccc4e0d2
beckermaxime84@gmail.com <beckermaxime84@gmail.com>
2022-10-26 15:04:23 +0200

    ajout app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var positions = [
+    { type: "TV", price: 400, marque: "Samsung" },
+    { type: "Ordinateur", price: 700 },
+    { type: "Téléphone", price: 200, marque: "Huawei" },
+    { type: "Portable", price: 900 },
+];
