a7241ad3c4e4512f3f5b8dd5f044304796004f72
beckermaxime84@gmail.com <beckermaxime84@gmail.com>
2022-10-26 15:13:15 +0200

    modification de la guitarre

--- app.ts
+++ app.ts
@@ -1,12 +1,31 @@
-type Produit = {
-    type: string;
+class Instrument {}
+
+/*  Déclaration de deux interfaces : Gclef  (clé de sol) & Fclef (clé de fa) */
+class Piano {
+
+    name: string;
     price: number;
-    marque?: string;
-   }
-    
-   let positions: Produit[] = [
-    { type: "TV", price:400, marque: "Samsung"},
-    { type: "Ordinateur", price: 700},
-    { type: "Téléphone", price: 200, marque:"Huawei"},
-    { type: "Portable", price: 900},
-   ]
+    priceSale: number;
+
+    constructor(name: string, price: number, priceSale: number) {
+        this.name = name;
+        this.price = price;
+        this.priceSale = priceSale;
+    }
+}
+
+
+class Guitare {
+
+    name: string;
+    price: number;
+    priceSale: number;
+    width: number;
+    lenght: number;
+
+    constructor(name: string, price: number, priceSale: number) {
+        this.name = name;
+        this.price = price;
+        this.priceSale = priceSale;
+    }
+}
